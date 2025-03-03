b6303fd8bb7643346c0afc10bca1ed07c337a3b8
fbfaustino <barros851@hotmail.com>
2025-03-03 18:55:49 -0300

    ajuste tab_filmes incremental

--- models/marts/tab_filmes.sql
+++ models/marts/tab_filmes.sql
@@ -9,7 +9,7 @@ with filmes as (
         a.nome as nome_filme,
         a.sinopse,
         a.popularidade,
-        a.data_lancamento,
+        TO_DATE(NULLIF(a.data_lancamento, ''), 'YYYY-MM-DD') AS data_lancamento,
         a.titulo_original,
         b.generos,
         c.palavra_chave_us || c.palavra_chave_br as palavras_chave,
